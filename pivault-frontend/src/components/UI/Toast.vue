6f9447cfad96c8cb125874734f71bc643952a05c
Satyarth5678 <satyarth.singh636@gmail.com>
2026-04-03 22:05:54 +0530

    Add UI components:Loader and Toast

---
+++ pivault-frontend/src/components/UI/Toast.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    v-if="visible"
+    class="fixed bottom-5 right-5 bg-green-500 text-white px-4 py-2 rounded shadow-lg"
+  >
+    {{ message }}
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const visible = ref(false);
+const message = ref("");
+
+const showToast = (msg) => {
+  message.value = msg;
+  visible.value = true;
+
+  setTimeout(() => {
+    visible.value = false;
+  }, 3000);
+};
+
+defineExpose({ showToast });
+</script>
